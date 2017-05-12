27dca9c6db7c6d214e149481992bd8b43752abec
Fabián <f.carrasco.marcos24@gmail.com>
2017-05-12 09:11:24 +0200

    Update CDVProviderDelegate.swift
    swift 3 change

--- src/ios/CDVProviderDelegate.swift
+++ src/ios/CDVProviderDelegate.swift
@@ -214,7 +214,7 @@ final class CDVProviderDelegate: NSObject, CXProviderDelegate {
          */
     }
 
-    private func postAudioNotification(_ message: String) {
+    fileprivate func postAudioNotification(_ message: String) {
         NotificationCenter.default.post(name: type(of: self).AudioNotification, object: message)
     }
 }
